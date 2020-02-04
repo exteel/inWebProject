3fb4b3a10b9524fad718380c2502beea9758a189
exteel <exteel9820@gmail.com>
2020-02-05 00:22:21 +0200

    Add files via upload

---
+++ src/assets/js/app.js
@@ -0,0 +1,8 @@
+//= ../../../node_modules/bootstrap/js/dist/util.js
+//= ../../../node_modules/bootstrap/js/dist/modal.js
+//= ../../../node_modules/slick-carousel/slick/slick.js
+
+$(function() {
+  //= components/slick.js
+  //= components/nav.js
+});
---
+++ src/assets/js/components/nav.js
@@ -0,0 +1,11 @@
+if (window.location.href.includes("index.html")) {
+  $("#firstnav").addClass("active");
+}
+if (window.location.href.includes("services.html")) {
+  $("#secondnav").addClass("active");
+}
+if (window.location.href.includes("portfolio.html")) {
+  $("#thirdnav").addClass("active");
+}
+
+
